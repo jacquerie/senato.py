a5fa3087b4ef2d92ef943d1532785d84de8ebe73
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2016-02-25 02:20:28 +0100

    Prefer downloads to immediate parsing.

--- senato/spiders/senato_spider.py
+++ senato/spiders/senato_spider.py
@@ -1,8 +1,15 @@
 # -*- coding: utf-8 -*-
 
+import os
+
+import re
+
 import scrapy
 
-from senato.items import Amendment
+
+AMENDMENT_XPATH = '//dl[@class="emendamenti"]/dt/a/@href'
+AMENDMENT_URL = '/japp/bgt/showdoc/17/EMEND/00{parent}/00{_id}/00{_id}.akn'
+HREF_REGEX = r'id=(?P<_id>\d+)&idoggetto=(?P<parent>\d+)'
 
 
 class SenatoSpider(scrapy.Spider):
@@ -17,52 +24,21 @@ class SenatoSpider(scrapy.Spider):
 
     def parse(self, response):
         """TODO."""
-        for href in response.xpath('//dl[@class="emendamenti"]/dt/a/@href'):
-            url = response.urljoin(href.extract())
-            yield scrapy.Request(url, callback=self.parse_amendment)
+        for href in response.xpath(AMENDMENT_XPATH).extract():
+            match = re.search(HREF_REGEX, href)
+            if match:
+                _id = match.group('_id')
+                parent = match.group('parent')
 
-    def parse_amendment(self, response):
+                relative_url = AMENDMENT_URL.format(_id=_id, parent=parent)
+                absolute_url = response.urljoin(relative_url)
+
+                yield scrapy.Request(absolute_url, callback=self.download_amendment)
+
+    def download_amendment(self, response):
         """TODO."""
-        ps = response.xpath('//div[@id="testo"]/p')
+        relative_filename = response.url.split('/')[-1]
+        absolute_filename = os.path.join(os.getcwd(), 'data', relative_filename)
 
-        amendment = Amendment()
-        amendment['_id'] = self._parse_id(ps[0])
-        amendment['authors'] = self._parse_authors(ps[1])
-        amendment['status'] = self._parse_status(ps[2])
-        amendment['text'] = self._parse_text(ps[3:])
-
-        yield amendment
-
-    def _parse_id(self, p):
-        """TODO."""
-        return p.xpath('b/text()').extract()[0]
-
-    def _parse_authors(self, p):
-        """TODO."""
-        return p.xpath('a/text()').extract()
-
-    def _parse_status(self, p):
-        """TODO."""
-        try:
-            status = p.xpath('b/text()').extract()[0]
-        except IndexError:
-            return 'PENDING'
-
-        if status == 'Respinta':
-            return 'REJECTED'
-        elif status == 'Ritirato':
-            return 'WITHDRAWN'
-        else:
-            return 'UNKNOWN'
-
-    def _parse_text(self, ps):
-        """TODO."""
-        result = []
-
-        for p in ps:
-            try:
-                result.append(p.xpath('text()').extract()[0].strip())
-            except IndexError:
-                pass
-
-        return '\n'.join(result)
+        with open(absolute_filename, 'wb') as f:
+            f.write(response.body)
